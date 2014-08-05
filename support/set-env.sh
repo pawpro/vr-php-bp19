5cfbc8bf3fc4f77318a8edeeaa23788a9f77f75f
Pawel Prochot <pawel@opigram.com>
2014-08-05 16:05:40 +0200

    Batardising couple of buildpacks

--- support/set-env.sh
+++ support/set-env.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-export S3_BUCKET="heroku-buildpack-php-tyler"
-
+export S3_BUCKET="yg-php-buildpack-opi"
 export LIBMCRYPT_VERSION="2.5.8"
-export LIFREETYPE_VERSION="2.4.12"
-export PHP_VERSION="5.4.17"
-export APC_VERSION="3.1.10"
-export PHPREDIS_VERSION="2.2.2"
-export LIBMEMCACHED_VERSION="1.0.7"
-export MEMCACHED_VERSION="2.0.1"
-export NEWRELIC_VERSION="2.9.5.78"
-
-export NGINX_VERSION="1.4.1"
-
-export EC2_PRIVATE_KEY=~/.ec2/pk.pem
-export EC2_CERT=~/.ec2/cert.pem
-export EC2_URL=https://ec2.us-east-1.amazonaws.com
+export LIBJPEG_VERSION="9a" 
+export PHP_VERSION="5.5.15"
+export LIBMEMCACHED_VERSION="1.0.18"
+export MEMCACHED_VERSION="2.2.0"
+export MEMCACHE_VERSION="3.0.8"
+export NEWRELIC_VERSION="4.10.1.62"
+export RABBITMQ_VERSION="1.4.0"
+export LIBRABBITMQ_VERSION="0.5.0"
+export OPENSSL_VERSION="0_9_8"
+export MEMCACHED_VERSION="1.4.20"
